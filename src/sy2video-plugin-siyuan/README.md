1c35f8746075f57d3561fd95c0949f0dfee92a13
崮生 <admin@shenzilong.cn>
2024-06-22 13:40:21 +0800

    通过 url 参数来让页面只显示指定的块     需要配置url参数

--- src/sy2video-plugin-siyuan/README.md
+++ src/sy2video-plugin-siyuan/README.md
@@ -1,3 +1,12 @@
 # 思源文档转视频插件
 
-本插件用于从思源笔记生成视频
+此插件用于辅助思源文档转视频
+
+目前提供以下功能：
+
+- 通过 url 参数来让页面只显示指定的块
+    需要配置url参数：
+
+    block_show (有此参数则功能生效)
+    block_id (块 id 数组)
+    例如：http://127.0.0.1:6809/stage/build/desktop/?r=iyqibyt&block_show=1&block_id=20240621163257-fp4jwg8
--- src/sy2video-plugin-siyuan/index.ts
+++ src/sy2video-plugin-siyuan/index.ts
@@ -1,7 +1,22 @@
-import {Plugin} from "siyuan";
+import { Plugin } from "siyuan";
 // 引入这个变量后 vite 会自动注入 hot
 import.meta.hot;
 
 export default class VitePlugin extends Plugin {
-
+  async onLayoutReady() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const blockId = urlParams.getAll("block_id");
+    const block_show = urlParams.get("block_show");
+    if (block_show && blockId) {
+      this.addFloatLayer({
+        x: 0,
+        y: 0,
+        ids: [...blockId],
+      });
+      const el: HTMLElement = window.siyuan.blockPanels[0].element;
+      //   钉住
+      (el.querySelector(`[data-type="pin"]`) as HTMLButtonElement)?.click();
+      await import("./view_block.css");
+    }
+  }
 }
