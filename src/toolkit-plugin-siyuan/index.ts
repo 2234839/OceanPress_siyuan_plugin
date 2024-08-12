400cf92296d2335d52ed158d3055aadbd73adbeb
崮生 <admin@shenzilong.cn>
2024-08-12 18:30:14 +0800

    添加反转功能

--- src/toolkit-plugin-siyuan/index.ts
+++ src/toolkit-plugin-siyuan/index.ts
@@ -10,26 +10,34 @@ type searchTagRes = {
     tags: string[];
   };
 };
-export default class VitePlugin extends SiyuanPlugin {
+export default class ToolKitPlugin extends SiyuanPlugin {
   tagSort = siyuan.bindData({
     initValue: {} as { [key: string]: number },
     that: this,
     storageName: "tagSort.json",
   });
+  toolkit_setting = siyuan.bindData({
+    initValue: { tag_sort_reverse: false },
+    that: this,
+    storageName: "toolkit_setting.json",
+  });
   onload(): void {
+    // @ts-ignore
+    globalThis["ToolKitPlugin"] = this;
     const oldFetch = globalThis.fetch;
     globalThis.fetch = async (input, init) => {
       const res = await oldFetch(input, init);
       if (input === "/api/search/searchTag") {
         const json = (await res.clone().json()) as searchTagRes;
-        json.data.tags = json.data.tags
-          .sort((a, b) => {
-            // todo 在搜索高亮的情况下等于 \u003cmark\u003eto\u003c/mark\u003edo
-            const a_key = a.replace(/<mark>(.*?)<\/mark>/g, "$1");
-            const b_key = b.replace(/<mark>(.*?)<\/mark>/g, "$1");
-            return (this.tagSort.value()[a_key] ?? 0) - (this.tagSort.value()[b_key] ?? 0);
-          })
-          .reverse();
+        json.data.tags = json.data.tags.sort((a, b) => {
+          // todo 在搜索高亮的情况下等于 \u003cmark\u003eto\u003c/mark\u003edo
+          const a_key = a.replace(/<mark>(.*?)<\/mark>/g, "$1");
+          const b_key = b.replace(/<mark>(.*?)<\/mark>/g, "$1");
+          return (this.tagSort.value()[a_key] ?? 0) - (this.tagSort.value()[b_key] ?? 0);
+        });
+        if (!this.toolkit_setting.value().tag_sort_reverse) {
+          json.data.tags.reverse();
+        }
         const newRes = new Response(JSON.stringify(json), {
           status: 200,
           headers: {
